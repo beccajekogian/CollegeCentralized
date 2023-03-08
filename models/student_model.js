a137c9c224c759eb0b51c861e736da7515bc1355
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2023-03-08 14:57:14 -0500

    yes

--- models/student_model.js
+++ models/student_model.js
@@ -34,23 +34,23 @@ exports.getStudent = function(studentName){
 
 exports.getCollegeList = async function(studentName){
   let students = JSON.parse(fs.readFileSync(__dirname+'/../data/students.json'));
-  console.log("hey" + students[studentName]["collegeList"]);
+  //console.log("hey" + students[studentName]["collegeList"]);
   let list = students[studentName]["collegeList"];
-  console.log("wow " + students[studentName].collegeList)
+  //console.log("wow " + students[studentName].collegeList)
   let collegeArray = [];
   for (college in list){
-    console.log("m" + college);
+    //console.log("m" + college);
     college.collegeName = college;
     collegeArray.push(list[college])
   }
-  console.log(collegeArray);
+  //console.log(collegeArray);
   return collegeArray;
 }
 
 exports.getSupplementDetails = async function(studentName, collegeName){
   let students = JSON.parse(fs.readFileSync(__dirname+'/../data/students.json'));
   // let colleges = JSON.parse(fs.readFileSync(__dirname+'/../data/colleges.json'));
-  console.log(students[studentName]["collegeList"][collegeName]);
+  console.log("yes" + students[studentName].collgeList);
   let supplements = students[studentName]["collegeList"][collegeName]["supplements"];
   for (supplement in supplements){
     console.log(supplement);
